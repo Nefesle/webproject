f53ac094715bd27af995cbb731f1cf010af859e9
Your Name <you@example.com>
2025-04-22 21:23:40 +0300

    Add login.html and update main.py

--- main.py
+++ main.py
@@ -15,5 +15,10 @@ def registration():
     return send_from_directory('html_files', 'registration.html')
 
 
+@app.route('/login')
+def login():
+    return send_from_directory('html_files', 'login.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
